2e014d6c16b42ca6dbea789efa27f0514819bb37
Michal <matej50@gmail.com>
2021-08-13 15:38:03 +0200

    screenshot from file & cli

--- screenshooter.js
+++ screenshooter.js
@@ -4,38 +4,66 @@ const Screenshot = async () => {
     const yargs = require('yargs/yargs')
     const {hideBin} = require('yargs/helpers')
     const argv = yargs(hideBin(process.argv)).argv
+    const width = 1200;
+    const height = 1800;
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    const fs = require('fs')
+    const filename = process.argv[2];
+    let config = '';
 
-    var date = new Date();
-    var dateId = date.toISOString().replace(/:/g, '-')
-
-    await page.goto(argv.url, {waitUntil: "domcontentloaded"});
-    await page.evaluate(() => {
-        window.scrollBy(0, 10);
-        var cookieConsent = document.getElementById('cc-card');
-        let expiry = new Date();
-        expiry.setTime(expiry.getTime() + (6 * 30 * 24 * 60 * 60 * 1000)); // six months
-        document.cookie = "cookieconsent_status=dismiss; expires=" + expiry.toUTCString();
-        cookieConsent.classList.add('uk-hidden');
-    })
-    await page.setViewport({
-        width: argv.width,
-        height: argv.height,
-    });
-    await page.waitForSelector(argv.selector);
-    var elements = await page.$$(argv.selector)
-
-    for (let i = 0; i < elements.length; i++) {
-        try {
-            if (await elements[i].$$eval(argv.subselector, el => el.length) > 0 || !argv.subselector) {
-                await elements[i].screenshot({path: `screenshot-${dateId}-${i} .png`})
-            }
-        } catch (e) {
-            console.log(e)
-        }
+    const cookie = async function () {
+        await page.evaluate(() => {
+            window.scrollBy(0, 10);
+            var cookieConsent = document.getElementById('cc-card');
+            cookieConsent.classList.add('uk-hidden');
+        })
     }
+    fs.readFile(filename, 'utf8', async function (err, data) {
+        config = data != null ? JSON.parse(data) : argv;
+        let lengthUrl = data != null ? config.urls.length : 1;
+        if (config.destination !== null && !fs.existsSync(`.${config.destination}`)) {
+            fs.mkdirSync(`.${config.destination}`)
+        }
+        var date = new Date();
+        var dateId = date.toISOString().replace(/:/g, '-')
 
-    await browser.close()
+        for (let i = 0; i < lengthUrl; i++) {
+            let url = config.urls ? config.urls[i].url : config.url
+            await page.goto(url, {waitUntil: "domcontentloaded"});
+            await page.setViewport({
+                width: config.viewportWidth ? config.viewportWidth : width,
+                height: config.viewportHeight ? config.viewportHeight : height
+            });
+
+            var testElement = await page.$$(`${config.testSelector}`)
+            if (testElement.length < 1) {
+                console.log(url)
+                continue;
+            }
+
+            await cookie();
+
+            try {
+                await page.waitForSelector(`${config.selector}`, {timeout: 5000})
+            } catch (error) {
+                console.log(error, url)
+                continue;
+            }
+
+            var elements = await page.$$(`${config.selector}`)
+
+            for (let j = 0; j < elements.length; j++) {
+                try {
+                    if (config.subselector == null || await elements[j].$$eval(`${config.subselector}`, el => el.length) > 0) {
+                        await elements[j].screenshot({path: `.${config.destination ? config.destination : ''}/${config.urls[i].screenshotPrefix ? config.urls[i].screenshotPrefix : 'screenshot'}-${dateId}-${i}${j}.png`})
+                    }
+                } catch (e) {
+                    console.log(e)
+                }
+            }
+        }
+        await browser.close()
+    });
 }
 Screenshot();
